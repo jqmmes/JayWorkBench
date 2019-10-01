a3c6f57a343ef5ba2397e5aaf22874465f51a355
joaquim <jqmmes@gmail.com>
2019-10-01 09:05:19 +0100

    to merge

--- workbench.py
+++ workbench.py
@@ -820,7 +820,7 @@ def main():
         print("===================================")
         for i in range(1, len(argv)):
             readConfig(argv[i])
-        EXPERIMENTS.sort(key=lambda e: e.devices+e.producers-e.request_time+len(e.cloudlets), reverse=False)
+        EXPERIMENTS.sort(key=lambda e: e.devices+e.producers-e.request_time+len(e.cloudlets), reverse=True)
         for e in EXPERIMENTS:
             runExperiment(e)
 
