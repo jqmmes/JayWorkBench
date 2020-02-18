bfb68811ec636a15c0c3c7d6611eacc8bff2864a
joaquim <jqmmes@gmail.com>
2020-02-18 14:52:52 +0000

    Cleanup and Updates

--- scripts/cloud_setup.sh
+++ scripts/cloud_setup.sh
@@ -14,20 +14,20 @@ sudo python3 -m pip install tensorflow
 sudo apt-get install -y stress
 sudo apt-get install -y iperf
 cd ~
-mkdir ~/ODCloud/
-mkdir ~/ODCloud/logs/
-mkdir ~/ODCloud/assets/
-cd ~/ODCloud/
-wget -x http://od-data.duckdns.org/apps/ODCloud.jar -O $HOME/ODCloud/ODCloud.jar
-wget -x http://od-data.duckdns.org/scripts/CloudletControl.py -O $HOME/ODCloud/CloudletControl.py
-mkdir -p $HOME/ODCloud/lib/protobuf/cloudlet
-wget -x http://od-data.duckdns.org/scripts/lib/protobuf/cloudlet/CloudletControl_pb2.py -O $HOME/ODCloud/lib/protobuf/cloudlet/CloudletControl_pb2.py
-wget -x http://od-data.duckdns.org/scripts/lib/protobuf/cloudlet/CloudletControl_pb2_grpc.py -O $HOME/ODCloud/lib/protobuf/cloudlet/CloudletControl_pb2_grpc.py
-cd ~/ODCloud/assets/
-wget -x http://od-data.duckdns.org/assets/sd/000000060521.jpg -O $HOME/ODCloud/assets/SD.jpg
-wget -x http://od-data.duckdns.org/assets/hd/C10_HD.jpg -O $HOME/ODCloud/assets/HD.jpg
-wget -x http://od-data.duckdns.org/assets/uhd/C14_UHD.jpg -O $HOME/ODCloud/assets/UHD.jpg
+mkdir ~/Jay-x86/
+mkdir ~/Jay-x86/logs/
+mkdir ~/Jay-x86/assets/
+cd ~/Jay-x86/
+wget -x http://od-data.duckdns.org/apps/Jay-x86.jar -O $HOME/Jay-x86/Jay-x86.jar
+wget -x http://od-data.duckdns.org/scripts/CloudletControl.py -O $HOME/Jay-x86/CloudletControl.py
+mkdir -p $HOME/Jay-x86/lib/protobuf/cloudlet
+wget -x http://od-data.duckdns.org/scripts/lib/protobuf/cloudlet/CloudletControl_pb2.py -O $HOME/Jay-x86/lib/protobuf/cloudlet/CloudletControl_pb2.py
+wget -x http://od-data.duckdns.org/scripts/lib/protobuf/cloudlet/CloudletControl_pb2_grpc.py -O $HOME/Jay-x86/lib/protobuf/cloudlet/CloudletControl_pb2_grpc.py
+cd ~/Jay-x86/assets/
+wget -x http://od-data.duckdns.org/assets/sd/000000060521.jpg -O $HOME/Jay-x86/assets/SD.jpg
+wget -x http://od-data.duckdns.org/assets/hd/C10_HD.jpg -O $HOME/Jay-x86/assets/HD.jpg
+wget -x http://od-data.duckdns.org/assets/uhd/C14_UHD.jpg -O $HOME/Jay-x86/assets/UHD.jpg
 echo "crontab -e"
-echo "@reboot tmux new -s odcloud -d  `which python3` $HOME/ODCloud/CloudletControl.py"
+echo "@reboot tmux new -s jay-x86 -d  `which python3` $HOME/Jay-x86/CloudletControl.py"
 echo "*/5 * * * * $HOME/duckdns/duck.sh >/dev/null 2>&1"
 echo "@reboot $HOME/duckdns/duck.sh >/dev/null 2>&1"
