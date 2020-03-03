e0d9eaded92fe456da10d28e48c16ef4f21bb726
joaquim <jqmmes@gmail.com>
2020-03-03 14:00:11 +0000

    Updated CloudSetup

--- scripts/cloud_setup.sh
+++ scripts/cloud_setup.sh
@@ -18,15 +18,15 @@ mkdir ~/Jay-x86/
 mkdir ~/Jay-x86/logs/
 mkdir ~/Jay-x86/assets/
 cd ~/Jay-x86/
-wget -x http://od-data.duckdns.org/apps/Jay-x86.jar -O $HOME/Jay-x86/Jay-x86.jar
-wget -x http://od-data.duckdns.org/scripts/CloudletControl.py -O $HOME/Jay-x86/CloudletControl.py
+wget -x https://www.dropbox.com/s/5esu529gqsxj6ha/Jay-x86.jar?dl=1 -O $HOME/Jay-x86/Jay-x86.jar
+wget -x https://www.dropbox.com/s/dmt97v19hlx54tw/CloudletControl.py?dl=1 -O $HOME/Jay-x86/CloudletControl.py
 mkdir -p $HOME/Jay-x86/lib/protobuf/cloudlet
-wget -x http://od-data.duckdns.org/scripts/lib/protobuf/cloudlet/CloudletControl_pb2.py -O $HOME/Jay-x86/lib/protobuf/cloudlet/CloudletControl_pb2.py
-wget -x http://od-data.duckdns.org/scripts/lib/protobuf/cloudlet/CloudletControl_pb2_grpc.py -O $HOME/Jay-x86/lib/protobuf/cloudlet/CloudletControl_pb2_grpc.py
+wget -x https://www.dropbox.com/s/x6qv8m315f5su7r/CloudletControl_pb2.py?dl=1 -O $HOME/Jay-x86/lib/protobuf/cloudlet/CloudletControl_pb2.py
+wget -x https://www.dropbox.com/s/crygm6ews8alclx/CloudletControl_pb2_grpc.py?dl=1 -O $HOME/Jay-x86/lib/protobuf/cloudlet/CloudletControl_pb2_grpc.py
 cd ~/Jay-x86/assets/
-wget -x http://od-data.duckdns.org/assets/sd/000000060521.jpg -O $HOME/Jay-x86/assets/SD.jpg
-wget -x http://od-data.duckdns.org/assets/hd/C10_HD.jpg -O $HOME/Jay-x86/assets/HD.jpg
-wget -x http://od-data.duckdns.org/assets/uhd/C14_UHD.jpg -O $HOME/Jay-x86/assets/UHD.jpg
+wget -x https://www.dropbox.com/s/1z4foixc8gmayi3/SD.jpg?dl=1 -O $HOME/Jay-x86/assets/SD.jpg
+wget -x https://www.dropbox.com/s/6ofu6h07bfjt0s3/HD.jpg?dl=1 -O $HOME/Jay-x86/assets/HD.jpg
+wget -x https://www.dropbox.com/s/7rkq106a78dv961/UHD.jpg?dl=1 -O $HOME/Jay-x86/assets/UHD.jpg
 echo "crontab -e"
 echo "@reboot tmux new -s jay-x86 -d  `which python3` $HOME/Jay-x86/CloudletControl.py"
 echo "*/5 * * * * $HOME/duckdns/duck.sh >/dev/null 2>&1"
